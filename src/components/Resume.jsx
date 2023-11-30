84cf9606fc7ffcb077abff038ab5697a7e1e335b
C_elare <houvillev@gmail.com>
2023-11-30 17:19:36 +0100

    feat(Resume): Add Resume component (master)

---
+++ src/components/Resume.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+
+const Resume = ({preview}) => {
+    return (
+        <>
+        <h1>Preview</h1>
+    <div>{preview}</div>
+        </>
+    )
+}
+
+Resume.propTypes = {
+    preview: PropTypes.object.isRequired,
+};
+export default Resume;
