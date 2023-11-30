ecd41dc0b6bbd987869e1d692298feeb90c2fac4
C_elare <houvillev@gmail.com>
2023-11-30 14:01:03 +0100

    feat(PersonalInformation): Add dynamic form fields (master)
    - Add dynamic form fields to the PersonalInformation component
    - Iterate over the formData keys to create form fields dynamically
    - Update the placeholder text to be more descriptive
    - Remove unnecessary comments and unused code
    
    Issue: N/A

--- src/components/PersonalInformation/FormPersonal.jsx
+++ src/components/PersonalInformation/FormPersonal.jsx
@@ -1,85 +1,46 @@
 import  { useState } from 'react'
 import PropTypes from "prop-types";
 
-const FormPersonal = ({onFormSubmit}) => {
-    const [formData, setFormData] = useState({
-        fullName : '',
-        address: '',
-        city: '',
-        phoneNumber: '',
-        email: ''
-    })
+const FormPersonal = ({ onFormSubmit, initialData }) => {
+    const [formData, setFormData] = useState(initialData);
 
     const handleChange = (event) => {
-        const {name, value} = event.target;
+        const { name, value } = event.target;
         setFormData(prevState => ({
             ...prevState,
-                [name]: value
-        }))
-    }
+            [name]: value
+        }));
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         onFormSubmit(formData);
-        setFormData({ fullName: '', address: '', city: '', phoneNumber: '', email: '' }); // Réinitialiser le formulaire
     };
-
     return (
         <>
         <form onSubmit={handleSubmit}>
-            <label>
-                <input
-                type="text"
-                name="fullName"
-                value={formData.fullName}
-                onChange={handleChange}
-                placeholder="Full Name"
-                />
-            </label>
-            <label>
-                <input
-                    type="text"
-                    name="city"
-                    value={formData.city}
-                    onChange={handleChange}
-                    placeholder="City"
-                />
-            </label>
-            <label>
-                <input
-                    type="text"
-                    name="address"
-                    value={formData.address}
-                    onChange={handleChange}
-                    placeholder="Address"
-                />
-            </label>
-            <label>
-                <input
-                    type="text"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    placeholder="Email"
-                />
-            </label>
-            <label>
-                <input
-                    type="text"
-                    name="phoneNumber"
-                    value={formData.phoneNumber}
-                    onChange={handleChange}
-                    placeholder="Phone Number"
-                />
-            </label>
-            <button type="submit">Submit</button>
+                {/* We can iterate over the formData keys to create form fields dynamically */}
+                {Object.keys(formData).map((key) => (
+                    <label key={key}>
+                        <input
+                            type="text"
+                            name={key}
+                            value={formData[key]}
+                            onChange={handleChange}
+                            placeholder={`Enter your ${key}`}
+                        />
+                        <br />
+                    </label>
+                ))}
+                <button type="submit">Submit</button>
         </form>
         </>
     )
 
 }
 FormPersonal.propTypes = {
-    onFormSubmit: PropTypes.func.isRequired
+    onFormSubmit: PropTypes.func.isRequired,
+   initialData: PropTypes.object.isRequired
 };
 
 export default FormPersonal;
