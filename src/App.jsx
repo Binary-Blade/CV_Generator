5fc2df2e91687bfb340c6dd9e90ddfcbb6e7d225
C_elare <houvillev@gmail.com>
2023-11-30 13:45:42 +0100

    feat: Add App.css and App.jsx files
    - Add App.css file with initial styles for the application
    - Add App.jsx file with basic structure and import ManagePersonal component
    
    Issue: main

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import  { useState } from 'react'
+import './App.css'
+import ManagePersonal from "./components/PersonalInformation/ManagePersonal.jsx";
+
+
+const App = () => {
+    return (
+        <>
+        <h1> Test </h1>
+        <ManagePersonal />
+        </>
+    )
+}
+
+export default App;
