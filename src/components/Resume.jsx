8b097d55eecca1b32d87f414fa211d2c7fbff170
C_elare <houvillev@gmail.com>
2023-12-03 11:07:56 +0100

    feat(Resume): Update Resume component styling (master)
    - Update Resume component styling to include a white background, shadow, and rounded corners
    - Add a heading with the text "Preview" in the center, with a font size of 3xl and bold font weight
    - Update the preview content to have a gray text color
    
    Closes #master

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,15 +1,16 @@
 import PropTypes from "prop-types";
 
-const Resume = ({preview}) => {
+const Resume = ({ preview }) => {
     return (
-        <>
-        <h1>Preview</h1>
-         <div>{preview}</div>
-        </>
-    )
-}
+        <div className="bg-white shadow-lg p-6 rounded-lg">
+            <h1 className="text-center text-3xl font-bold text-gray-700 mb-8">Preview</h1>
+            <div className="text-gray-800">{preview}</div>
+        </div>
+    );
+};
 
 Resume.propTypes = {
     preview: PropTypes.object.isRequired,
 };
+
 export default Resume;
