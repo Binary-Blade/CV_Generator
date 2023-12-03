7bf9a4338d63d046b0eeb92316f088cb6208b5d9
C_elare <houvillev@gmail.com>
2023-12-03 11:02:27 +0100

    feat(Form): improve form input styling and add submit button (master)

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -4,18 +4,21 @@ const Form = ({formData, onChange}) => {
     return (
         <>
         {Object.keys(formData).map((key) => (
-                <label key={key}>
-                    <input
-                        type="text"
-                        name={key}
-                        value={formData[key]}
-                        onChange={onChange}
-                        placeholder={`Your ${key}`}
-                    />
-                    <br />
-                </label>
-            ))}
-    <button type="submit">Submit</button>
+            <label key={key} className="block mb-4">
+                <span className="text-gray-700 capitalize">{key}</span>
+                <input
+                    type="text"
+                    name={key}
+                    value={formData[key]}
+                    onChange={onChange}
+                    placeholder={`Enter your ${key}`}
+                    className="mt-1 block w-full p-2 border border-gray-300 rounded"
+                />
+            </label>
+        ))}
+            <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+                Submit
+            </button>
         </>
     )
 }
